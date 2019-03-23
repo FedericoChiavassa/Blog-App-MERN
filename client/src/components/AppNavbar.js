48ad45f9a1d340436bd1d5cae9bb94e225210ea3
Fede <federico.chiavassa@hotmail.it>
2019-03-23 18:03:04 +0100

    React Router Update

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import {
     Collapse,
     Navbar,
@@ -21,20 +22,26 @@ class AppNavbar extends Component {
         });
     }
 
+    close = () => {
+        this.setState({
+          isOpen: false
+        });
+    }
+
     render() {
         return (
             <div>
                 <Navbar color="dark" dark expand="sm" className="mb-5">
                     <Container>
-                        <NavbarBrand href="/">BloggApp</NavbarBrand>
+                        <NavbarBrand tag={Link} to="/" onClick={this.close}>BloggApp</NavbarBrand>
                         <NavbarToggler onClick={this.toggle}></NavbarToggler>
-                        <Collapse isOpen={this.state.isOpen} navbar>
+                        <Collapse isOpen={this.state.isOpen} onClick={this.toggle} navbar>
                             <Nav className="mr-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="/posts">Posts</NavLink>
+                                    <NavLink tag={Link} to="/posts">Posts</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink href="/about">About</NavLink>
+                                    <NavLink tag={Link} to="/about">About</NavLink>
                                 </NavItem>
                             </Nav>
                             <Nav className="ml-auto" navbar>
